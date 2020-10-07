1726e90baddcb053f0e1ab23188772e2f61639ba
Dadson Papa Dadze <pddadson@gmail.com>
2020-10-07 21:47:01 +0000

    Emailing on contact page

--- src/components/Contact/ContactForm.vue
+++ src/components/Contact/ContactForm.vue
@@ -12,14 +12,47 @@
         <h4 class="form-title">Send us a message</h4>
         <form action="" class="flex-column">
           <label for="name">Name</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" v-model="name" />
           <label for="email" name="email">Email</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" v-model="email" />
           <label for="message">Message</label>
-          <textarea name="message" id="" rows="5"></textarea>
-          <input type="submit" value="Send Message" />
+          <textarea name="message" id="" v-model="message" rows="5"></textarea>
+          <input
+            type="submit"
+            @click.prevent="sendEmail"
+            value="Send Message"
+          />
         </form>
       </section>
     </div>
   </div>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+    };
+  },
+  methods: {
+    async sendEmail() {
+      try {
+        var message = await Email.send({
+          SecureToken: "425562e1-92fd-4b1e-b63d-214263ab6211",
+          To: "pddadson@gmail.com",
+          From: this.email,
+          Subject: this.name,
+          Body: this.email,
+        });
+        console.log(message);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
